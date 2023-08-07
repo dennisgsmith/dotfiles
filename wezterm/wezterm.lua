e73badd2aa88c4e2ae56e937dee90f6e29b86e9d
Dennis Smith <dennisgsmith12@gmail.com>
2023-08-07 14:17:47 -0400

    mor colors

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm';
 
 local LIGHT_COLORSCHEME = 'Github (base16)'
-local LIGHT_BORDER = 'light grey'
+local LIGHT_BORDER = '#f0f0f0'
 local LIGHT_CURSOR = 'black'
 local DARK_COLORSCHEME = 'Github Dark'
 local DARK_BORDER = '#303030'
@@ -32,8 +32,8 @@ local cursor_bg = scheme_for_appearance(a, LIGHT_CURSOR, DARK_CURSOR)
 
 return {
   -- Fonts
-  font     	= wezterm.font_with_fallback({"JetBrains Mono", "termicons"}), -- [JetBrains Mono]
-  font_size	= 13.0, -- [12.0]
+  font = wezterm.font_with_fallback({"JetBrains Mono", "termicons"}), -- [JetBrains Mono]
+  font_size = 13.0, -- [12.0]
 
   -- Colors
   color_scheme  = color_scheme,
@@ -41,24 +41,85 @@ return {
   cursor_blink_ease_in = 'Constant',
   cursor_blink_ease_out = 'Constant',
   cursor_blink_rate = 600,
-
   colors = {
-    cursor_bg = cursor_bg
+    cursor_bg = cursor_bg,
+    tab_bar = {
+      -- The active tab is the one that has focus in the window
+      active_tab = {
+	-- The color of the background area for the tab
+	bg_color = scheme_for_appearance(a, '#fdfdfd', '#222222'),
+	-- The color of the text for the tab
+	fg_color = scheme_for_appearance(a, '#0f0f0f', '#f0f0f0'),
+
+	-- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+	-- label shown for this tab.
+	-- The default is "Normal"
+	intensity = 'Half',
+
+	-- Specify whether you want "None", "Single" or "Double" underline for
+	-- label shown for this tab.
+	-- The default is "None"
+	underline = 'None',
+
+	-- Specify whether you want the text to be italic (true) or not (false)
+	-- for this tab.  The default is false.
+	italic = false,
+
+	-- Specify whether you want the text to be rendered with strikethrough (true)
+	-- or not for this tab.  The default is false.
+	strikethrough = false,
+      },
+
+      -- Inactive tabs are the tabs that do not have focus
+      inactive_tab = {
+	bg_color = scheme_for_appearance(a, '#e6e6e6', '#444444'),
+	fg_color = scheme_for_appearance(a, '#0f0f0f', '#e0e0e0'),
+
+	-- The same options that were listed under the `active_tab` section above
+	-- can also be used for `inactive_tab`.
+      },
+
+      -- You can configure some alternate styling when the mouse pointer
+      -- moves over inactive tabs
+      inactive_tab_hover = {
+	bg_color = scheme_for_appearance(a, '#d0d0d0', '#a0a0a0'),
+	fg_color = scheme_for_appearance(a, '#ffffff', '#0a0a0a'),
+	italic = true,
+
+	-- The same options that were listed under the `active_tab` section above
+	-- can also be used for `inactive_tab_hover`.
+      },
+
+      -- The new tab button that let you create new tabs
+      new_tab = {
+	bg_color = border_color,
+	fg_color = scheme_for_appearance(a, '#808080', '#a0a0a0'),
+
+	-- The same options that were listed under the `active_tab` section above
+	-- can also be used for `new_tab`.
+      },
+
+      -- You can configure some alternate styling when the mouse pointer
+      -- moves over the new tab button
+      new_tab_hover = {
+	bg_color = border_color,
+	fg_color = cursor_bg,
+	italic = true,
+
+	-- The same options that were listed under the `active_tab` section above
+	-- can also be used for `new_tab_hover`.
+      },
+    },
   },
 
   -- Appearance
+  window_decorations = "RESIZE|MACOS_FORCE_ENABLE_SHADOW",
+  window_frame = {
+    active_titlebar_bg = border_color,
+  },
+
   window_background_opacity   	= 0.85,  -- [1.0] alpha channel value with floating point numbers
   macos_window_background_blur  = 100,   -- blur
-  enable_tab_bar              	= false, -- [true]
   hide_tab_bar_if_only_one_tab	= false, -- [false] hide the tab bar when there is only a single tab in the window
-
-  window_frame = {
-    border_left_width = '0.25cell',
-    border_right_width = '0.25cell',
-    border_bottom_height = '0.10cell',
-    border_left_color = border_color,
-    border_right_color = border_color,
-    border_bottom_color = border_color,
-  }
 }
 
