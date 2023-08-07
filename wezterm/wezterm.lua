0a233df6360127ec4d11d8218b64c6022f90e766
Dennis Smith <smith_dennis@bah.com>
2023-08-07 11:57:45 -0400

    border

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,11 @@
 local wezterm = require 'wezterm';
 
 local LIGHT_COLORSCHEME = 'Github (base16)'
+local LIGHT_BORDER = 'light grey'
+local LIGHT_CURSOR = 'black'
 local DARK_COLORSCHEME = 'Github Dark'
+local DARK_BORDER = '#303030'
+local DARK_CURSOR = '#ffffff'
 
 -- wezterm.gui is not available to the mux server, so take care to
 -- do something reasonable when this config is evaluated by the mux
@@ -13,13 +17,18 @@ local function get_appearance()
 end
 
  -- full list @ wezfurlong.org/wezterm/colorschemes/index.html
-local function scheme_for_appearance(appearance)
+local function scheme_for_appearance(appearance, light_theme, dark_theme)
   if appearance:find 'Dark' then
-    return DARK_COLORSCHEME
+    return dark_theme
   else
-    return LIGHT_COLORSCHEME
+    return light_theme
   end
 end
+
+local a = get_appearance()
+local color_scheme = scheme_for_appearance(a, LIGHT_COLORSCHEME, DARK_COLORSCHEME)
+local border_color = scheme_for_appearance(a, LIGHT_BORDER, DARK_BORDER)
+local cursor_bg = scheme_for_appearance(a, LIGHT_CURSOR, DARK_CURSOR)
 
 return {
   -- Fonts
@@ -27,16 +36,29 @@ return {
   font_size	= 12.0                                                       	, -- [12.0]
 
   -- Colors
-  color_scheme  = scheme_for_appearance(get_appearance()),
+  color_scheme  = color_scheme,
   default_cursor_style = 'BlinkingBlock',
   cursor_blink_ease_in = 'Constant',
   cursor_blink_ease_out = 'Constant',
   cursor_blink_rate = 600,
 
+  colors = {
+    cursor_bg = cursor_bg
+  },
+
   -- Appearance
-  window_background_opacity   	= 0.85  	, -- [1.0] alpha channel value with floating point numbers
-  macos_window_background_blur  = 100	, -- blur
-  enable_tab_bar              	= false	, -- [true]
-  hide_tab_bar_if_only_one_tab	= false	, -- [false] hide the tab bar when there is only a single tab in the window
+  window_background_opacity   	= 0.85,  -- [1.0] alpha channel value with floating point numbers
+  macos_window_background_blur  = 100,   -- blur
+  enable_tab_bar              	= false, -- [true]
+  hide_tab_bar_if_only_one_tab	= false, -- [false] hide the tab bar when there is only a single tab in the window
+
+  window_frame = {
+    border_left_width = '0.25cell',
+    border_right_width = '0.25cell',
+    border_bottom_height = '0.10cell',
+    border_left_color = border_color,
+    border_right_color = border_color,
+    border_bottom_color = border_color,
+  }
 }
 
