4066523e6743f9c2c124b59ffeb3216775811481
Dennis Smith <dennisgsmith12@gmail.com>
2023-08-09 08:09:30 -0400

    use lualine for buffer mgmt

--- nvim/init.lua
+++ nvim/init.lua
@@ -130,7 +130,10 @@ local config = {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
-      extensions = { 'nvim-tree', 'lazy', 'fugitive' }
+      extensions = { 'nvim-tree', 'lazy', 'fugitive' },
+      tabline = {
+        lualine_a = { 'buffers' },
+      }
     },
     event = "VimEnter",
     lazy = false,
@@ -195,19 +198,43 @@ local config = {
 local opts = {}
 require('lazy').setup(config, opts)
 require('nvim-tree').setup({})
-require('bufferline').setup({
-  options = {
-    offsets = {
-      {
-        filetype = "NvimTree",
-        text = "File Explorer",
-        text_align = "left",
-        separator = true
-      }
-    },
-  }
+
+-- keeps track of buffers that have been "touched" (entered insert mode or modified the buffer)
+local id = vim.api.nvim_create_augroup("startup", {
+  clear = false
 })
 
+local persistbuffer = function(bufnr)
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
+  vim.fn.setbufvar(bufnr, 'bufpersist', 1)
+end
+
+vim.api.nvim_create_autocmd({ "BufRead" }, {
+  group = id,
+  pattern = { "*" },
+  callback = function()
+    vim.api.nvim_create_autocmd({ "InsertEnter", "BufModifiedSet" }, {
+      buffer = 0,
+      once = true,
+      callback = function()
+        persistbuffer()
+      end
+    })
+  end
+})
+
+vim.keymap.set('n', '<Leader>bcu',
+  function()
+    local curbufnr = vim.api.nvim_get_current_buf()
+    local buflist = vim.api.nvim_list_bufs()
+    for _, bufnr in ipairs(buflist) do
+      if vim.bo[bufnr].buflisted and bufnr ~= curbufnr and (vim.fn.getbufvar(bufnr, 'bufpersist') ~= 1) then
+        vim.cmd('bd ' .. tostring(bufnr))
+      end
+    end
+  end, { silent = true, desc = '[B]uffers [c]lose [u]nused' })
+
+-- set hlslens keymaps
 local map = vim.api.nvim_set_keymap
 local kopts = { noremap = true, silent = true }
 map('n', 'n',
