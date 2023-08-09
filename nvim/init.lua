11997936a63cfaa3d0296d58a1944f6c4a294ad1
Dennis Smith <smith_dennis@bah.com>
2023-08-08 23:02:51 -0400

    vertical help keymap

--- nvim/init.lua
+++ nvim/init.lua
@@ -185,8 +185,9 @@ local config = {
     config = function()
       vim.opt.termguicolors = true
     end,
-    opts = {
-    },
+    keys = {
+      { "<leader>cb", ":sil bdelete! %" }
+    }
   },
   require 'core.plugins.autoformat',
   require 'core.plugins.debug',
@@ -256,6 +257,8 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+vim.keymap.set('n', '<leader>vh', ':vertical bo help ', { silent = true })
 
 --- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
