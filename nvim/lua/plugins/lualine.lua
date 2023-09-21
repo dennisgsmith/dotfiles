cfe228f9d735fc7a30f1e4502aeb2f5d6c3b0fde
Dennis Smith <smith_dennis@bah.com>
2023-09-21 11:24:02 -0400

    lualine show relative path

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -7,6 +7,9 @@ local M = {
       globalstatus = true,
     },
     extensions = { 'lazy', 'fugitive' },
+    sections = {
+      lualine_c = { { 'filename', path = 1 } }
+    },
   },
   config = function(_, opts)
     require('lualine').setup(opts)
