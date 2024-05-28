20b5c690c949e323c3712d99cc26f833ce22d529
Dennis Smith <smith_dennis@bah.com>
2024-05-28 08:10:02 -0400

    configure nvim smart splits w/o terminal integration for now

---
+++ nvim/lua/plugins/smart-splits.lua
@@ -0,0 +1,10 @@
+return {
+  'mrjones2014/smart-splits.nvim',
+  lazy = false,
+  keys = {
+    { '-', '<cmd>SmartResizeLeft<cr>', desc = 'Resize Left' },
+    { '_', '<cmd>SmartResizeDown<cr>', desc = 'Resize Down' },
+    { '+', '<cmd>SmartResizeUp<cr>', desc = 'Resize Up' },
+    { '=', '<cmd>SmartResizeRight<cr>', desc = 'Resize Right' },
+  },
+}
