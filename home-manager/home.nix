1caa73350825702d32eb98f12f3386e50969f1e7
Dennis Smith <dennisgsmith12@gmail.com>
2024-08-20 21:41:41 -0400

    initial nix-darwin config

--- home-manager/home.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, ... }: {
-  home.username = builtins.getEnv "USER";
-  home.homeDirectory = builtins.getEnv "HOME";
-  home.stateVersion = "22.11";
-  programs.home-manager.enable = true;
-}
