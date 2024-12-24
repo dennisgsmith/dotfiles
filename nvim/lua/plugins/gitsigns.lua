f7607b148b419c98c84d79d27085b79c493dffdf
Dennis Smith <dennisgsmith12@gmail.com>
2024-12-24 16:34:01 -0500

    replace webdev icons with mini.icons

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,4 +1,16 @@
-return {
+local function previous_hunk()
+  require('gitsigns').nav_hunk 'prev'
+end
+
+local function next_hunk()
+  require('gitsigns').nav_hunk 'next'
+end
+
+local function preview_hunk()
+  require('gitsigns').preview_hunk()
+end
+
+local M = {
   'lewis6991/gitsigns.nvim',
   opts = {
     signs = {
@@ -9,9 +21,9 @@ return {
       changedelete = { text = '~' },
     },
     on_attach = function(bufnr)
-      vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk, { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
-      vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
-      vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
+      vim.keymap.set('n', '<leader>gp', previous_hunk, { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
+      vim.keymap.set('n', '<leader>gn', next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
+      vim.keymap.set('n', '<leader>ph', preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
     end,
   },
   config = function()
@@ -20,3 +32,5 @@ return {
   end,
   dependencies = { 'petertriho/nvim-scrollbar', 'f-person/auto-dark-mode.nvim' },
 }
+
+return M
