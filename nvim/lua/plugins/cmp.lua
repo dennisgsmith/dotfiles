d91cf0976e2ad64edd6053252d9946170360433d
Dennis Smith <dennisgsmith12@gmail.com>
2024-12-25 14:22:22 -0500

    fix mini icons cmp lookup

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -30,7 +30,7 @@ return {
       formatting = {
         format = function(entry, vim_item)
           if vim.tbl_contains({ 'path' }, entry.source.name) then
-            local icon, hl_group = require('mini.icons').get_icon(entry:get_completion_item().label)
+            local icon, hl_group = require('mini.icons').get(entry:get_completion_item().data.stat.type, entry:get_completion_item().label)
             if icon then
               vim_item.kind = icon
               vim_item.kind_hl_group = hl_group
